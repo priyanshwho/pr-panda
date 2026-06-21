b39088b85604d4d03c379ea3cfd73de23afc8cb1
priyanshwho <priyanshu82711@gmail.com>
2026-06-22 01:48:01 +0530

    sync repo from code base done

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,9 +1,10 @@
 import { inngest } from "@/features/inngest/client";
 import { reviewPullRequest } from "@/features/reviews/server/review-pr-function";
+import { syncRepoCodebaseFunction } from "@/features/repo-sync/server/repo-sync-function";
 import { serve } from "inngest/next";
 import { processTask } from "./function";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [processTask, reviewPullRequest],
+  functions: [processTask, reviewPullRequest,syncRepoCodebaseFunction],
 });
